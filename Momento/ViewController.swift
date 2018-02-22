a1d6caa08561e0e4a7e11623101ec3ea88982186
EricaLong <Erica@erica-longs-macbook-air-2.local>
2018-02-21 20:09:15 -0500

    Initial Commit

---
+++ Momento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Momento
+//
+//  Created by EricaLong on 2018-02-21.
+//  Copyright © 2018 EricaLong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
